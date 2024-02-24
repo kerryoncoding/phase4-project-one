37891d52a25d7c2a75d8297f3b401c2025ac9f38
Kerry Guarino <kerry.guarino@gmail.com>
2024-02-24 15:09:39 -0500

    initial commit

---
+++ server/migrations/versions/bd17e17fa00e_initial_db.py
@@ -0,0 +1,24 @@
+"""initial db
+
+Revision ID: bd17e17fa00e
+Revises: 
+Create Date: 2024-02-24 15:09:15.520239
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'bd17e17fa00e'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
