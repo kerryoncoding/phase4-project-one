70fea2811ea3cb8c83a4d0e1cd1ed71695333010
Kerry Guarino <kerry.guarino@gmail.com>
2024-02-25 17:28:53 -0500

    updated models with __repr__

--- server/models.py
+++ server/models.py
@@ -4,17 +4,23 @@ from sqlalchemy import MetaData
 db = SQLAlchemy(metadata=metadata)
 
 class Squad(db.Model):
-    __tablename__ = 'Squads'
+   __tablename__ = 'Squads'
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    description = db.Column(db.String)
+   id = db.Column(db.Integer, primary_key=True)
+   name = db.Column(db.String)
+   description = db.Column(db.String)
+
+   def __repr__(self):
+        return f'<Squad {self.id}, {self.name}, {self.description}>'
 
 
 class User(db.Model):
-    __tablename__ = 'users'
+   __tablename__ = 'users'
 
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False, index=True)
-    email = db.Column(db.String(120), unique=True)
-    verified = db.Column(db.Boolean, default=False)
+   id = db.Column(db.Integer, primary_key=True)
+   username = db.Column(db.String(80), unique=True, nullable=False, index=True)
+   email = db.Column(db.String(120), unique=True)
+   verified = db.Column(db.Boolean, default=False)
+
+   def __repr__(self):
+        return f'<User {self.id}, {self.username}, {self.email}, {self.verified}>'
--- server/seed.py
+++ server/seed.py
@@ -4,4 +4,3 @@ if __name__ == "__main__":
   with app.app_context():
     pass
     # remove pass and write your seed data
-    # test
