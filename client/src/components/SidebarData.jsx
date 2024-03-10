8d0ae526cebea2270639b7f00c6ab42567240ad9
Kerry Guarino <kerry.guarino@gmail.com>
2024-03-09 19:23:25 -0500

    added components folder, copied sidebar

---
+++ client/src/components/SidebarData.jsx
@@ -0,0 +1,38 @@
+
+import React from "react";
+import * as FaIcons from "react-icons/fa";
+import * as AiIcons from "react-icons/ai";
+import * as IoIcons from "react-icons/io";
+
+export const SidebarData = [
+  {
+      title: " Home",
+      path: "/",
+      icon: <AiIcons.AiFillHome />,
+      cName: "nav-text",
+   },
+   {
+      title: "Squads",
+      path: "/squads",
+      //  icon: <IoIcons.IoIosPaper />,
+      cName: "nav-text",
+  },
+  {
+      title: "About",
+      path: "/about",
+      //  icon: <IoIcons.IoIosPaper />,
+      cName: "nav-text",
+  },
+  {
+      title: "Create",
+      path: "/create",
+      //  icon: <FaIcons.FaCartPlus />,
+      cName: "nav-text",
+   },
+   {
+      title: "Login",
+      path: "/login",
+      //  icon: <FaIcons.FaCartPlus />,
+      cName: "nav-text",
+  },
+];
